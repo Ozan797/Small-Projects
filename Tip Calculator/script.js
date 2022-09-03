849bed1dfd0900afca955374f3fc3beba5ab025d
Ozan Gokberk <ozan8@hotmail.co.uk>
2022-09-03 15:53:01 +0300

    added event listeners

---
+++ Tip Calculator/script.js
@@ -0,0 +1,8 @@
+const sliders = document.querySelectorAll("input[type='range']");
+sliders.forEach((slider) => {
+  slider.addEventListener("input", calculateTip);
+});
+
+function calculateTip(){
+
+}
