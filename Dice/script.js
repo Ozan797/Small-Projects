377c58a072557a3bbf9374ed27494aac65e47569
Ozan Gokberk <ozan8@hotmail.co.uk>
2022-09-05 15:22:34 +0300

    Formated

--- Dice/script.js
+++ Dice/script.js
@@ -2,7 +2,7 @@ const body = document.querySelector("body");
 const rollValue = document.querySelector("#roll-value");
 const btn = document.querySelector("#btn");
 
-btn.onclick = () =>{
+btn.onclick = () => {
   let randomNum = Math.floor(Math.random() * 6) + 1;
   rollValue.innerHTML = randomNum;
 };
