4d612a0dca95e36305585979ad84d37fdfdd9f96
Ozan Gokberk <ozan8@hotmail.co.uk>
2022-09-05 15:20:56 +0300

    Added Styling

--- Dice/script.js
+++ Dice/script.js
@@ -1,8 +1,8 @@
-const body = document.querySelector('body');
-const rollValue = document.querySelector('#roll-value');
-const btn = document.querySelector('#btn')
+const body = document.querySelector("body");
+const rollValue = document.querySelector("#roll-value");
+const btn = document.querySelector("#btn");
 
-btn.addEventListener('click', () => {
-    let randomNum = Math.floor(Math.random() * 6) + 1
-    rollValue.innerHTML = randomNum
-})
+btn.onclick = () =>{
+  let randomNum = Math.floor(Math.random() * 6) + 1;
+  rollValue.innerHTML = randomNum;
+};
